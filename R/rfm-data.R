805ac9db8725645e953f055f91f20ecf699b2731
rsquaredin <rsquaredin@gmail.com>
2017-10-16 11:38:16 +0530

    example data

---
+++ R/rfm-data.R
@@ -0,0 +1,14 @@
+#' RFM Data Set
+#'
+#' A dataset containing transactions of different customers.
+#'
+#' @format A tibble with 25613 rows and 4 variables:
+#' \describe{
+#'   \item{order_date}{order date}
+#'   \item{order_id}{order id}
+#'   \item{customer_id}{customer id}
+#'   \item{revenue}{transaction amount}
+#' }
+#'
+#' @source https://github.com/joaolcorreia/RFM-analysis
+"rfm_data"
