8d4889a8fc038df86126b95874d12619f52536fb
rsquaredin <rsquaredin@gmail.com>
2017-10-16 11:40:48 +0530

    utility functions

--- R/utils.R
+++ R/utils.R
@@ -1,203 +1,112 @@
-library(readr)
-library(lubridate)
-library(dplyr)
-library(magrittr)
-library(forcats)
-library(purrr)
-library(ggplot2)
+#' @importFrom dplyr enquo bind_rows
+#' @importFrom stats quantile
+#' @importFrom magrittr extract add %<>% %>% use_series
+#' @importFrom purrr prepend
+#' @importFrom assertthat are_equal
+#' @importFrom forcats fct_unique
+bins <- function(data, value, n_bins) {
 
-# read data
-rfm_data <- readr::read_csv('https://raw.githubusercontent.com/blast-analytics-marketing/RFM-analysis/master/sample-orders.csv')
+  my_value <- enquo(value)
 
-# view data
-rfm_data
+  length_out <- n_bins + 1
+  result <- data %>%
+    pull(!!my_value) %>%
+    quantile(probs = seq(0, 1, length.out = length_out)) %>%
+    unname() %>%
+    `extract`(c(-1, -length_out)) %>%
+    add(1)
 
-# now
-input_date <- as_datetime('2014-04-01 05:30:00', tz = 'UTC')
+  return(result)
 
-# change datetime class
-rfm_data %<>%
-  mutate(
-    ord_time = mdy_hm(order_date)
-  ) %>%
-  select(ord_time, order_id, customer, grand_total) %>%
-  rename(order_date = ord_time, order_id = order_id,
-         customer_id = customer, revenue = grand_total)
-
-# export data
-write.csv(rfm_data, 'rfm_data.csv')
-
-rfm_table <- rfm_data %>%
-  select(customer_id, order_date, revenue) %>%
-  group_by(customer_id) %>%
-  summarise(date_most_recent = min(order_date), amount = sum(revenue),
-            transaction_count = n()) %>%
-  mutate(
-    recency_days = (input_date - date_most_recent) / ddays()
-  ) %>%
-  select(customer_id, date_most_recent, recency_days, transaction_count,
-         amount)
-
-# monetary_score
-no_of_bins <- 5
-length_out <- no_of_bins + 1
-bins <- rfm_table %>%
-  pull(amount) %>%
-  quantile(probs = seq(0, 1, length.out = length_out)) %>%
-  unname() %>%
-  `extract`(c(-1, -length_out)) %>%
-  add(1)
-
-bins_lower <- rfm_table %>%
-  pull(amount) %>%
-  min() %>%
-  append(bins)
-
-bins_upper <- rfm_table %>%
-  pull(amount) %>%
-  max %>%
-  add(1) %>%
-  prepend(bins)
-
-rfm_table$monetary_score <- NA
-for (i in seq_len(no_of_bins)) {
-  rfm_table$monetary_score[rfm_table$amount >= bins_lower[i] &
-                             rfm_table$amount < bins_upper[i]] <- i
 }
 
+bins_lower <- function(data, value, bins) {
 
-rfm_table %>%
-  group_by(monetary_score) %>%
-  summarise(count = n())
+  my_value <- enquo(value)
 
+  result <- data %>%
+    pull(!!my_value) %>%
+    min() %>%
+    append(bins)
 
-# recency score
-bins <- rfm_table %>%
-  pull(recency_days) %>%
-  quantile(probs = seq(0, 1, length.out = length_out)) %>%
-  unname() %>%
-  `extract`(c(-1, -length_out)) %>%
-  add(1)
+  return(result)
 
-bins_lower <- rfm_table %>%
-  pull(recency_days) %>%
-  min() %>%
-  append(bins)
-
-bins_upper <- rfm_table %>%
-  pull(recency_days) %>%
-  max %>%
-  add(1) %>%
-  prepend(bins)
-
-rfm_table$recency_score <- NA
-for (i in seq_len(no_of_bins)) {
-  rfm_table$recency_score[rfm_table$recency_days >= bins_lower[i] &
-                             rfm_table$recency_days < bins_upper[i]] <- i
 }
 
-rfm_table %>%
-  group_by(recency_score) %>%
-  summarise(count = n())
+bins_upper <- function(data, value, bins) {
 
+  my_value <- enquo(value)
 
-# frequency_score
-bins <- rfm_table %>%
-  pull(transaction_count) %>%
-  quantile(probs = seq(0, 1, length.out = length_out)) %>%
-  unname() %>%
-  `extract`(c(-1, -length_out)) %>%
-  add(1)
+  result <- data %>%
+    pull(!!my_value) %>%
+    max() %>%
+    add(1) %>%
+    prepend(bins)
 
-bins_lower <- rfm_table %>%
-  pull(transaction_count) %>%
-  min() %>%
-  append(bins)
+  return(result)
 
-bins_upper <- rfm_table %>%
-  pull(transaction_count) %>%
-  max %>%
-  add(1) %>%
-  prepend(bins)
-
-rfm_table$frequency_score <- NA
-for (i in seq_len(no_of_bins)) {
-  rfm_table$frequency_score[rfm_table$transaction_count >= bins_lower[i] &
-                            rfm_table$transaction_count < bins_upper[i]] <- i
 }
 
-rfm_table %>%
-  group_by(frequency_score) %>%
-  summarise(count = n())
+heatmap_data <- function(rfm_table) {
+
+  result <- rfm_table %>%
+    use_series(rfm) %>%
+    group_by(frequency_score, recency_score) %>%
+    select(frequency_score, recency_score, amount) %>%
+    summarise(monetary = mean(amount))
+
+  l_frequency <- check_levels(result, frequency_score)
+  l_recency <- check_levels(result, recency_score)
+
+  levels_frequency <- check_levels(result, frequency_score) %>% length
+  levels_recency <- check_levels(result, recency_score) %>% length
+
+  f_frequency <- rfm_table %>%
+    use_series(frequency_bins)
+
+  if (!are_equal(levels_frequency, f_frequency)) {
+    result %<>%
+      modify_rfm(., f_frequency, l_frequency)
+  }
+
+  r_recency <- rfm_table %>%
+    use_series(recency_bins)
+
+  if (!are_equal(levels_recency, r_recency)) {
+    result %<>%
+      modify_rfm(., r_recency, l_recency)
+  }
 
 
-# rfm_score
-rfm_table %<>%
-  mutate(
-    rfm_score = recency_score * 100 + frequency_score * 10 + monetary_score
-  ) %>%
-  select(customer_id, date_most_recent, recency_days, transaction_count, amount,
-         recency_score, frequency_score, monetary_score, rfm_score)
 
-# heat map data
-rfm_heatmap <- rfm_table %>%
-  group_by(frequency_score, recency_score) %>%
-  select(frequency_score, recency_score, amount) %>%
-  summarise(monetary = mean(amount))
+  return(result)
 
-check_levels <- rfm_heatmap %>%
-  pull(frequency_score) %>%
-  as.factor() %>%
-  fct_unique()
+}
 
-missing <- !(seq_len(no_of_bins) %in% check_levels )
-missing2 <- seq_len(no_of_bins)[missing]
-extra_data <- expand.grid(missing2, 1:5, 0)
-names(extra_data) <- names(rfm_heatmap)
-rfm_heatmap %<>%
-  bind_rows(extra_data)
+check_levels <- function(heatmap_data, column) {
 
-# heat map
-ggplot(data = rfm_heatmap) +
-  geom_tile(aes(x = frequency_score, y = recency_score, fill = monetary)) +
-  scale_fill_gradientn(limits = c(0, 300), colours = topo.colors(5, alpha = 0.5),
-    breaks = c(0, 50, 100,  150, 200, 250, 300),
-    name = 'Mean Monetary Value') +
-  ggtitle('RFM Heat Map') + xlab('Frequency') + ylab('Recency')
+  my_column <- enquo(column)
 
-# bar chart
-ggplot(data = rfm_table) +
-  geom_bar(aes(x = frequency_score), fill = 'blue') +
-  facet_grid(recency_score ~ ., switch = 'x') +
-  xlab('Frequency Score') + ylab('Record Count')
+  result <- heatmap_data %>%
+    pull(!!my_column) %>%
+    as.factor() %>%
+    fct_unique() %>%
+    as.vector() %>%
+    as.integer()
 
-# scatter plots
-# monetary vs recency
-ggplot(rfm_table) +
-  geom_point(aes(x = amount, y = recency_days),
-             color = 'blue') +
-  xlab('Monetary') + ylab('Recency')
+  return(result)
 
-# monetary vs frequency
-ggplot(rfm_table) +
-  geom_point(aes(x = amount, y = transaction_count),
-    color = 'blue') +
-  xlab('Monetary') + ylab('Frequency')
+}
 
-# frequency vs recency
-ggplot(rfm_table) +
-  geom_point(aes(x = transaction_count, y = recency_days),
-             color = 'blue') +
-  xlab('Frequency') + ylab('Recency')
+modify_rfm <- function(heatmap_data, n_bins, check_levels) {
 
-# test
-output <- read_csv('rfm_spss_output.csv')
-output %>%
-  group_by(Monetary_score) %>%
-  summarise(count = n())
-output %>%
-  group_by(Recency_score) %>%
-  summarise(count = n())
-output %>%
-  group_by(Frequency_score) %>%
-  summarise(count = n())
+  missing <- !(seq_len(n_bins) %in% check_levels)
+  missing2 <- seq_len(n_bins)[missing]
+  extra_data <- expand.grid(missing2, seq_len(n_bins), 0)
+  names(extra_data) <- names(heatmap_data)
+  heatmap_data %<>%
+    bind_rows(extra_data)
+
+  return(heatmap_data)
+
+}
