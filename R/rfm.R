db64b35b32710163bb84df6b1c442aac8139d608
rsquaredin <rsquaredin@gmail.com>
2017-10-16 11:40:27 +0530

    add global variables

---
+++ R/rfm.R
@@ -0,0 +1,13 @@
+#' \code{rfm} package
+#'
+#' Recency, Frequency and Monetary Value Analysis
+#'
+#' @docType package
+#' @name rfm
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "amount",
+  "date_most_recent", "frequency_bins", "frequency_score", "monetary",
+  "monetary_score", "recency_bins", "recency_days", "recency_score",
+  "rfm", "rfm_score", "transaction_count"))
