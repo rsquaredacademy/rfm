bf3d3fdca025847c4763db6df60bcb360759a0bd
rsquaredin <rsquaredin@gmail.com>
2017-12-07 20:55:58 +0530

    run tests

--- tests/testthat/test_rfm_table.R
+++ tests/testthat/test_rfm_table.R
@@ -2,13 +2,13 @@ context('rfm_table')
 
 test_that('output from rfm_table is as expected', {
 
-  analysis_date <- lubridate::as_datetime('2014-04-01 05:30:00', tz = 'UTC')
+  analysis_date <- lubridate::as_date('2006-12-31', tz = 'UTC')
   result <- rfm_table(rfm_data, customer_id, order_date, revenue, analysis_date)
   actual <- result %>%
     use_series(rfm) %>%
     extract2('transaction_count') %>%
     sum
-  expected <- 25613
+  expected <- 4906
   expect_equal(actual, expected)
 
 })
