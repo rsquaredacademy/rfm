c4b66aa065d9189afa56ebceeadb324dce753fa0
rsquaredin <rsquaredin@gmail.com>
2018-03-31 13:03:59 +0530

    fixes #3

--- R/rfm-data.R
+++ R/rfm-data.R
@@ -1,4 +1,4 @@
-#' RFM Data Set
+#' RFM transaction data
 #'
 #' A dataset containing transactions of different customers.
 #'
@@ -10,3 +10,18 @@
 #' }
 #'
 "rfm_data_orders"
+
+#' RFM customer data
+#'
+#' A dataset containing customer level data.
+#'
+#' @format A tibble with 39,999 rows and 5 variables:
+#' \describe{
+#'   \item{customer_id}{Customer id.}
+#'   \item{total_amount}{Total amount of all orders.}
+#'   \item{most_recent_visit}{Date of the most recent transaction.}
+#'   \item{number_of_purchases}{Total number of transactions/orders.}
+#'   \item{purchase_interval}{Number of days since last transaction/order.}
+#' }
+#'
+"rfm_data_customer"
