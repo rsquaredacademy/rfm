d4fa329b09d22d4d3eb7b80a6bfb4d27e7ec3519
rsquaredin <rsquaredin@gmail.com>
2018-04-06 13:11:17 +0530

    improve code coverage

--- tests/testthat/test-rfm-plots.R
+++ tests/testthat/test-rfm-plots.R
@@ -43,3 +43,41 @@ test_that('output from rfm_fm_plot is as expected', {
   vdiffr::expect_doppelganger('rfm fmplot', p$plot)
 
 })
+
+
+test_that('output from rfm_heatmap is as expected', {
+
+  skip_on_cran()
+
+  analysis_date <- lubridate::as_date('2006-12-31', tz = 'UTC')
+  rfm_result <- rfm_table_order(rfm_data_orders, customer_id, order_date,
+    revenue, analysis_date)
+  p <- rfm_heatmap(rfm_result)
+  vdiffr::expect_doppelganger('rfm heatmap', p$plot)
+
+})
+
+test_that('output from rfm_histograms is as expected', {
+
+  skip_on_cran()
+
+  analysis_date <- lubridate::as_date('2006-12-31', tz = 'UTC')
+  rfm_result <- rfm_table_order(rfm_data_orders, customer_id, order_date,
+    revenue, analysis_date)
+  p <- rfm_histograms(rfm_result)
+  vdiffr::expect_doppelganger('rfm histograms', p$plot)
+
+})
+
+test_that('output from rfm_order_dist is as expected', {
+
+  skip_on_cran()
+
+  analysis_date <- lubridate::as_date('2006-12-31', tz = 'UTC')
+  rfm_result <- rfm_order <- rfm_table_order(rfm_data_orders, customer_id, order_date,
+    revenue, analysis_date)
+  p <- rfm_order_dist(rfm_result)
+  vdiffr::expect_doppelganger('rfm orderdist', p$plot)
+
+})
+
